3bf688ceb9a5f2c6a64aa2bae5999a3b572d0119
mattmcdermott <mcdermott@berkeley.edu>
2019-03-26 16:36:37 -0700

    Improved awareness of VASP DFPT run in electronic convergence check

--- pymatgen/io/vasp/outputs.py
+++ pymatgen/io/vasp/outputs.py
@@ -563,7 +563,7 @@ class Vasprun(MSONable):
         ionic step
         """
         final_esteps = self.ionic_steps[-1]["electronic_steps"]
-        if 'LEPSILON' in self.incar and self.incar['LEPSILON']:
+        if 'IBRION' in v.incar and (v.incar['IBRION'] == 7 or v.incar['IBRION'] == 8):
             i = 1
             to_check = set(['e_wo_entrp', 'e_fr_energy', 'e_0_energy'])
             while set(final_esteps[i].keys()) == to_check:
